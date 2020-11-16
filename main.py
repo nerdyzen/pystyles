36e5292304c32f0b40a75514c36ab12dedd1d87f
nerdyzen <54746912+nerdyzen@users.noreply.github.com>
2020-11-15 17:35:17 -0700

    -

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class styles:
 	HEADER = '\033[95m'
-	ENDC = '\033[0m'
+	END = '\033[0m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 class colors:
 	BLUE = '\033[34m'
 	CYAN = '\033[96m'
 	GREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
+	ORANGE = '\033[93m'
+	RED = '\033[91m'
+	END = '\033[0m'
